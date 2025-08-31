c6c8b60ca0ecdc95460dfa6b46282104402d7015
gardenmore34-dotcom <gardenmore34@gmail.com>
2025-09-01 01:58:49 +0530

    Update CategoryCardsSection.jsx
    api to API

--- src/components/CategoryCardsSection.jsx
+++ src/components/CategoryCardsSection.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getAllCategories } from '../api/categoryApi';
+import { getAllCategories } from '../api/categoryAPI';
 import { useNavigate } from 'react-router-dom';
 
 const CategoryCardsSection = () => {
